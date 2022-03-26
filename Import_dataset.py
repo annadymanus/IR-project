4fc756c5db4d1043efadde717a4faa892113a138
Nil Palau <nil.palau01@estudiant.upf.edu>
2022-03-26 19:54:16 +0100

    implemented a buffer based solution for reading large .gz files

---
+++ Import_dataset.py
@@ -0,0 +1,12 @@
+import gzip
+
+
+def import_dataset():
+    file_name = 'msmarco-docs.trec.gz'
+    f = gzip.open(file_name, 'r') #we read from a buffer so we never load the 8 GB in memory
+    for line in f:
+        print(line)
+    f.close()
+
+if __name__ == "__main__":
+    import_dataset()
