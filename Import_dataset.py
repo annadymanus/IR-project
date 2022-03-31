df4a652f5e4b0f204b4e17c502d6f81e1bc4e230
Nil Palau <nil.palau01@estudiant.upf.edu>
2022-03-31 17:31:21 +0100

    Implemented a function to print N_lines from the dataset or N_full_texts

--- Import_dataset.py
+++ Import_dataset.py
@@ -1,12 +1,42 @@
 import gzip
 
 
-def import_dataset():
+def print_dataset(number_of_lines):
     file_name = 'msmarco-docs.trec.gz'
     f = gzip.open(file_name, 'r') #read from a buffer so we never load the 8 GB in memory
-    for line in f:
-        print(line)
+    i = 0
+    number_of_texts = 0
+    for line in f: #line is type python_bytes
+        if(i< number_of_lines):
+            line_to_string = line.decode("utf-8")
+            if(line_to_string == "\n"):
+                number_of_texts += 1
+            print(line_to_string[:-1])
+            i+=1
+        else:
+            break
     f.close()
+    print("Number of total printed texts: ", number_of_texts)
+
+
+def print_n_texts(number_of_texts):
+    file_name = 'msmarco-docs.trec.gz'
+    f = gzip.open(file_name, 'r') #read from a buffer so we never load the 8 GB in memoryç
+    number_of_texts_counter = 0
+    for line in f: #line is type python_bytes
+        if(number_of_texts_counter < number_of_texts):
+            line_to_string = line.decode("utf-8")
+            if(line_to_string == "\n"):
+                number_of_texts_counter += 1
+            print(line_to_string[:-1])
+        else:
+            break
+    f.close()
+    if(number_of_texts == number_of_texts_counter):
+        print("Succesfully printed", number_of_texts,"texts")
+    else:
+        print("Text printing ERROR!")
+
 
 if __name__ == "__main__":
-    import_dataset()
+    print_n_texts(3)
