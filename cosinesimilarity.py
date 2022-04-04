0febfbf811da495fb1f487c5065587ad54bce7b3
annadymanus <aniadymanus@gmail.com>
2022-04-04 10:15:51 +0200

    changed the labels to actual labels (as instructed in evaluation metrics). Sorted the documents for each key based on their cosine similarity

--- cosinesimilarity.py
+++ cosinesimilarity.py
@@ -22,13 +22,9 @@ def compute_cosine_similarity(dict_data):
             query_tfidf = document[1].reshape(1, -1) #reshape to "fake" 2Dvector [] --> [[]]
             docu_tfidf = document[3].reshape(1, -1) #reshape to "fake" 2Dvector [] --> [[]]
             cos_val = cosine_similarity(query_tfidf, docu_tfidf)
-            if float(cos_val) >= 0.5:
-                cosine_results.append([document, cos_val, 1])
-                print('RELEVANT')
-            else:
-                cosine_results.append([document, cos_val, 0])
-                print('NON RELEVANT')
-        dict_cosine_val[key] = cosine_results
+            cosine_results.append([document[0], cos_val, document[-1]]) #add actual label from the input data (important for evaluation metrics)
+        dict_cosine_val[key] = cosine_results.sorted(key=lambda x: x[1]) #sort doctuments by their cosine similarity for each key
+
         print("################## QUEST " + key + " SUCCESFULL ##################")
     with open('cosine_similarity_TEST_results', 'wb') as handle: #save cosinesimilarity results to a file using pickle
         pickle.dump(dict_cosine_val, handle, protocol=pickle.HIGHEST_PROTOCOL)
